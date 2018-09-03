5d891ec145b021353f2ab6ae4c8f55f5b1e62e94
traviswheeler100580065 <100580065@student.swin.edu.au>
2018-09-03 15:13:36 +1000

    Added validation, cant submit blank or non numeric fields. Echo messages alert.

--- insert.php
+++ insert.php
@@ -14,14 +14,21 @@
   $productID = $_POST['productID'];
   $quantity = $_POST['quantity'];
 
-  $sql = "INSERT INTO php_database.sales(orderID, productID, date, quantity) values ('$saleID', '$productID', current_timestamp, '$quantity')";
-
-  if($conn->query($sql) === TRUE){
-    echo "New Sales record created successfully";
+  if(!is_numeric($saleID)){
+    echo "Sale ID must be a number";
+  } else if(!is_numeric($productID)){
+    echo "Product ID must be a number";
+  } else if(!is_numeric($quantity)){
+    echo "Quantity must be a number";
   } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    $sql = "INSERT INTO php_database.sales(orderID, productID, date, quantity) values ('$saleID', '$productID', current_timestamp, '$quantity')";
+
+    if($conn->query($sql) === TRUE){
+      echo "New Sales record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
   }
-
-  $conn->close();
-
 ?>
