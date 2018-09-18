31a9b71bf7249f0187e7248672020fb3f27148f6
Cyrus Edgren <101606991@student.swin.edu.au>
2018-09-18 15:11:26 +1000

    Added additional filters to filter_functions.php for date, expiration date, and quantity.

--- filter_functions.php
+++ filter_functions.php
@@ -19,8 +19,62 @@ function filter_category($db, $table, $category) {
     else {
         $sql = "SELECT * FROM $table WHERE category = '$category'";
         $result = mysqli_query($db, $sql);
-
     }
         return $result;
 }
 
+function filter_date($db, $table, $date) {
+    //Returns a selection based on the date given.
+
+    $result = NULL;
+
+    //The product table is the only table with the 'category' column, so here is a safety check.
+    if($table != 'purchases' && $table != 'sales') echo "Invalid table selection";
+
+    else {
+        if ($table == 'purchases') {
+            $sql = "SELECT * FROM $table WHERE purchase_date = '$date'";
+        } else {
+            $sql = "SELECT * FROM $table WHERE recordDate = '$date'";
+        }
+        $result = mysqli_query($db, $sql);
+    }
+    return $result;
+}
+
+
+function order_quantity($db, $table) {
+    //Returns a selection ordered by the quantity of the items.
+
+    $result = NULL;
+
+    //The product table is the only table with the 'category' column, so here is a safety check.
+    if($table != 'inventory' && $table != 'purchases' && $table != 'sales') echo "Invalid table selection";
+
+    else {
+        if ($table == 'inventory') {
+            $sql = "SELECT * FROM $table ORDER BY total_quantity";
+        } else if ($table == 'purchases') {
+            $sql = "SELECT * FROM $table ORDER BY quantity_remaining";
+        } else {
+            $sql = "SELECT * FROM $table ORDER BY quantity";
+        }
+        $result = mysqli_query($db, $sql);
+    }
+    return $result;
+}
+
+function order_expiration_date($db, $table) {
+    //Returns a selection ordered by the expiration date of the items.
+
+    $result = NULL;
+
+    //The product table is the only table with the 'category' column, so here is a safety check.
+    if($table != 'purchases') echo "Invalid table selection";
+
+    else {
+        $sql = "SELECT * FROM $table ORDER BY expiry_date";
+        $result = mysqli_query($db, $sql);
+    }
+    return $result;
+}
